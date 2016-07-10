158c70bb4316f015f0b8e39d69b93782b851540a
Martin Delille <martin@phonations.com>
2016-07-10 20:16:31 +0200

    add label and location

--- PilotFishEmitter/ViewController.swift
+++ PilotFishEmitter/ViewController.swift
@@ -7,12 +7,19 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    var locationManager: CLLocationManager? = nil
+
+    @IBOutlet weak var longitudeLabel: UILabel!
+    @IBOutlet weak var latitudeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.locationManager = CLLocationManager()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func locate(sender: AnyObject) {
+        if let lm = self.locationManager {
+            lm.delegate = self
+            lm.desiredAccuracy = kCLLocationAccuracyBest
+            lm.startUpdatingLocation()
+        }
+    }
 
+    func locationManager(manager: CLLocationManager, didUpdateToLocation newLocation: CLLocation, fromLocation oldLocation: CLLocation) {
+        self.longitudeLabel.text = String(newLocation.coordinate.longitude)
+        self.latitudeLabel.text = String(newLocation.coordinate.latitude)
+    }
 }
 
