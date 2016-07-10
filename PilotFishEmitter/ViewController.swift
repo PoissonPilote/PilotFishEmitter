ab4591eb8d5c8bbe0d861433b41472bab6c4ef1d
Martin Delille <martin@phonations.com>
2016-07-10 20:01:04 +0200

    Initial Commit

---
+++ PilotFishEmitter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PilotFishEmitter
+//
+//  Created by Martin Delille on 10/07/2016.
+//  Copyright © 2016 Phonations. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
